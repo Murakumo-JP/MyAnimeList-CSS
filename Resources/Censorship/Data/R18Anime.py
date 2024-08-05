f20c14d072afdcfb71b26319ef2c42bcd9339bac
Murakumo <32967450+Murakumo-JP@users.noreply.github.com>
2024-08-05 23:02:55 +0000

    Action Test

--- Resources/Censorship/Data/R18Anime.py
+++ Resources/Censorship/Data/R18Anime.py
@@ -1,26 +1,32 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import os
 
 animelist = []
 page = 1
+
+file_path = 'R18Anime.css'
+if not os.path.exists(file_path):
+    open(file_path, 'w').close()
+
 while True:
-    request = requests.get("https://myanimelist.net/anime/genre/12/Hentai?page=" + str(page))
+    request = requests.get(f"https://myanimelist.net/anime/genre/12/Hentai?page={page}")
     soup = BeautifulSoup(request.text, 'html.parser')
     links = soup.find_all("a", class_="link-title")
-    
-    if(len(links)):
+
+    if links:
         page += 1
     else:
         break
-        
+
     for link in links:
         temp = re.sub(r'(?:.*?myanimelist\.net)', '', link.get('href'))
         temp = re.sub(r'\/[^\/]*?$', '', temp)
-        animelist.append('.data.image a[href^="'+temp+'/"]:before{content: var(--R18);}')
+        animelist.append(f'.data.image a[href^="{temp}/"]:before{{content: var(--R18);}}')
 
-        f = open('R18Anime.css', 'w')
+with open(file_path, 'w') as f:
     for index in animelist:
         f.write(index + '\n')
-    print(animelist)
-f.close()
+
+print(animelist)
